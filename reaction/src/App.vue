b191c2ebd1c90c1fea4e9e680829dbc3c5f1c057
Eduardo <ealvarez@carminaticonsulting.com>
2021-10-10 21:39:02 -0500

    project init

---
+++ reaction/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Vue Reaction Timer</h1>>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #4444;
+  margin-top: 60px;
+}
+</style>
